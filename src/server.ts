8c53516434270525fbc97d25a504e298903d236f
Emillio Martins <ciberlei@gmail.com>
2026-08-04 22:19:07 -0300

    refactor:alterar resposta para erros novos

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,13 @@
 import express from "express";
+import { pinoHttp } from "pino-http";
+import logger from "./lib/logger.ts";
 import errorHandle from "./middlewares/errorHandler.ts";
-import requestLogger from "./middlewares/requestLogger.ts";
 import CustomerRouter from "./routes/customer.routes.ts";
 import InvoiceRouter from "./routes/invoice.routes.ts";
 
 const app = express();
 
-app.use(requestLogger);
+app.use(pinoHttp({ logger }));
 
 app.use(express.json());
 
